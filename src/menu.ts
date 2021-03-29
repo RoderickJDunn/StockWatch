2cd4536e83dc1ed148a10daa6575d28d9ab72e14
RoderickJDunn <roderickjdunn@gmail.com>
2021-03-29 16:02:31 -0400

    Fix app name

--- src/menu.ts
+++ src/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Stock Watch',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Stock Watch',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
